3fdc81cd378bb503ae2f314cd1cc27caefdbe376
Mody <143101587+Source-Ze@users.noreply.github.com>
2023-11-11 22:09:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ import asyncio
 import os, asyncio, re
 from os import system
 from telethon.tl.types import ChannelParticipantsAdmins, ChannelParticipantAdmin, ChannelParticipantCreator
-client = TelegramClient('SourceZe', 21627756, "fe77fbf0cae9f7f5ece37659e2466cf1").start(bot_token=Config.TG_BOT_TOKEN)
+client = TelegramClient('SourceZe', api_id=Config.APP_ID, api_hash=Config.API_HASH).start(bot_token=Config.TG_BOT_TOKEN)
 from telethon import TelegramClient as tg
 from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest as pc, JoinChannelRequest as join, LeaveChannelRequest as leave, DeleteChannelRequest as dc
 from telethon.sessions import StringSession as ses
@@ -24,9 +24,6 @@ mybot = os.environ.get("N444NBOT")
 bot = borg = client
 
 SourceZe = 6509622797
-
-api_id = Config.APP_ID 
-api_hash = Config.API_HASH 
 
 Bot_Username =(mybot , None) or "N444NBOT"
 
